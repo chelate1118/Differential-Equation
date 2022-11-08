34a687daa6b965fce89e050743d2d77b2dd3d491
Ryan Lee <gs21077@gs.hs.kr>
2022-11-08 11:55:07 +0900

    from_str monomial todo: from_str polynomial

--- src/equation.rs
+++ src/equation.rs
@@ -1,14 +1,19 @@
+use std::error::Error;
+
 mod polynomial;
 
 pub(super) trait Equation {
     fn value(&self, x: f64, y: f64) -> f64;
+    fn from_str(from: &str) -> Result<Self, Box<dyn Error>>
+    where
+        Self: Sized;
 }
 
 #[test]
 fn test_monomial() {
     use crate::assert_feq;
 
-    let mono: Box<dyn Equation> = Box::new(
+    let mono1: Box<dyn Equation> = Box::new(
         polynomial::Monomial {
             coeff: 0.8,
             x_exp: 0.5,
@@ -16,25 +21,38 @@ fn test_monomial() {
         }
     );
 
-    assert_feq!(mono.value(4.0, -1.0), 1.6);
-    assert_feq!(mono.value(4.0, 0.0), 0.0);
+    assert_feq!(mono1.value(4.0, -1.0), 1.6);
+    assert_feq!(mono1.value(4.0, 0.0), 0.0);
+
+    let mono2 = polynomial::Monomial::from_str("0.8");
+    let mono3 = polynomial::Monomial::from_str("0.8x");
+    let mono4 = polynomial::Monomial::from_str("3x2y-3");
+
+    assert!(matches!(mono2, Err(_)));
+    assert!(matches!(mono3, Err(_)));
+    assert!(matches!(mono4, Ok(_)));
+
+    let mono4 = mono4.unwrap();
+
+    assert_feq!(mono4.value(1.0, 1.0), 3.0);
+    assert_feq!(mono4.value(2.0, -1.0), -12.0);
 }
 
 #[test]
 fn test_polynomial() {
     use crate::assert_feq;
 
-    let poly: Box<dyn Equation> = Box::new(
+    let poly1: Box<dyn Equation> = Box::new(
         polynomial::Polynomial {
             formula: vec!(
-                polynomial::Monomial::from((0.8, 0.5, 2.0)),
-                polynomial::Monomial::from((-1.0, 1.0, 1.0)),
-                polynomial::Monomial::from((0.0, -5.0, 2.0)),
-                polynomial::Monomial::from((4.0, 0.0, 0.0)),
+                Box::new(polynomial::Monomial::from((0.8, 0.5, 2.0))),
+                Box::new(polynomial::Monomial::from((-1.0, 1.0, 1.0))),
+                Box::new(polynomial::Monomial::from((0.0, -5.0, 2.0))),
+                Box::new(polynomial::Monomial::from((4.0, 0.0, 0.0))),
             )
         }
     );
 
-    assert_feq!(poly.value(4.0, -1.0), 9.6);
-    assert_feq!(poly.value(4.0, 0.0), 4.0);
+    assert_feq!(poly1.value(4.0, -1.0), 9.6);
+    assert_feq!(poly1.value(4.0, 0.0), 4.0);
 }
--- src/equation/polynomial.rs
+++ src/equation/polynomial.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::io::Error as IOError;
+use std::io::ErrorKind;
+
 use super::Equation;
 
 pub(super) struct Monomial {
@@ -10,23 +14,50 @@ impl Equation for Monomial {
     fn value(&self, x: f64, y: f64) -> f64 {
         self.coeff * x.powf(self.x_exp) * y.powf(self.y_exp)
     }
+
+    fn from_str(from: &str) -> Result<Self, Box<dyn Error>>
+    where
+        Self: Sized,
+    {
+        if let Some((split_x1, split_x2)) = from.split_once('x') {
+            if let Some((split_y1, split_y2)) = split_x2.split_once('y') {
+                Ok(Monomial {
+                    coeff: split_x1.parse()?,
+                    x_exp: split_y1.parse()?,
+                    y_exp: split_y2.parse()?,
+                })
+            } else {
+                Err(Box::new(IOError::new(ErrorKind::InvalidInput, "no y")))
+            }
+        } else {
+            Err(Box::new(IOError::new(ErrorKind::InvalidInput, "no x")))
+        }
+    }
 }
 
 impl From<(f64, f64, f64)> for Monomial {
     fn from(tuple: (f64, f64, f64)) -> Self {
-        Monomial { coeff: tuple.0, x_exp: tuple.1, y_exp: tuple.2 }
+        Monomial {
+            coeff: tuple.0,
+            x_exp: tuple.1,
+            y_exp: tuple.2,
+        }
     }
 }
 
 pub(super) struct Polynomial {
-    pub(super) formula: Vec<Monomial>
+    pub(super) formula: Vec<Box<dyn Equation>>,
 }
 
 impl Equation for Polynomial {
     fn value(&self, x: f64, y: f64) -> f64 {
-        self.formula
-            .iter()
-            .map(|mono| mono.value(x, y))
-            .sum()
+        self.formula.iter().map(|mono| mono.value(x, y)).sum()
+    }
+
+    fn from_str(from: &str) -> Result<Self, Box<dyn Error>>
+    where
+        Self: Sized,
+    {
+        todo!()
     }
 }
